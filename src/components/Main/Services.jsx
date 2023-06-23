26fca551144da9e93cee5b74517a57dbc8899e59
binisha <binisha.bajracharya@webo.digital>
2023-06-23 08:46:06 +0545

    added shadow

--- src/components/Main/Services.jsx
+++ src/components/Main/Services.jsx
@@ -2,94 +2,94 @@ import React from 'react';
 
 export default function Services() {
   return (
-    <main className='service_section'>
-      <section className=' service-section  w-full max-w-[1525px] mx-auto mt-[164.35px] '>
-        <h2 className='text-center font-bold text-[25px]'>
+    <main className="service_section">
+      <section className=" service-section  w-full max-w-[1525px] mx-auto mt-[164.35px] ">
+        <h2 className="text-center font-bold text-[25px]">
           Our Service and Prices
         </h2>
-        <p className='text-center pt-5 pb-16 max-w-4xl mx-auto'>
+        <p className="text-center pt-5 pb-16 max-w-4xl mx-auto">
           ContentAlpha offers optimal solutions at affordable rates for every
           content need. Understanding your requirements and generating results
           that satisfy you is our core mission. With us, you will be able to get
           the maximum possible ROI.
         </p>
-        <section className='services flex gap-11 justify-center flex-wrap mx-auto items-center max-w-[1032px]'>
-          <div className=' article_service w-[250px] h-64 p-5 border-2 rounded-xl border-[#267BFF]'>
-            <img src='../images/blue file.png' alt='' />
-            <h4 className='text-[#267BFF] font-bold pt-5 pb-3'>
+        <section className="services flex gap-11 justify-center flex-wrap mx-auto items-center max-w-[1032px]">
+          <div className=" article_service w-[250px] h-64 p-5 border-2 rounded-xl border-[#267BFF]">
+            <img src="../images/blue file.png" alt="" />
+            <h4 className="text-[#267BFF] font-bold pt-5 pb-3">
               Article Writing
             </h4>
-            <p className='text-[#999999] pb-5'>
+            <p className="text-[#999999] pb-5">
               Lörem ipsum polysam spetila pyde. Trapolig regnbågsbarn och beligt
               ehylig viktigt
             </p>
-            <a href=''>
-              <img src='../images/blue arrow.png' alt='' />
+            <a href="">
+              <img src="../images/blue arrow.png" alt="" />
             </a>
           </div>
-          <div className=' content_services w-[250px] h-64 p-5 border-2 rounded-xl border-[#21DAFF]'>
-            <img src='../images/cyan file.png' alt='' />
-            <h4 className='text-[#21DAFF] font-bold pt-5 pb-3'>
+          <div className=" content_services w-[250px] shadow-md  h-64 p-5 border-2 rounded-xl border-[#21DAFF]">
+            <img src="../images/cyan file.png" alt="" />
+            <h4 className="text-[#21DAFF] font-bold pt-5 pb-3">
               Content Writing
             </h4>
-            <p className='text-[#999999] pb-5'>
+            <p className="text-[#999999] pb-5">
               Lörem ipsum polysam spetila pyde. Trapolig regnbågsbarn och beligt
               ehylig viktigt
             </p>
-            <a href=''>
-              <img src='../images/cyan arrow.png' alt='' />
+            <a href="">
+              <img src="../images/cyan arrow.png" alt="" />
             </a>
           </div>
-          <div className='ebook_services w-[250px] h-64 p-5 border-2 rounded-xl border-[#FF834B]'>
-            <img src='../images/orange file.png' alt='' />
-            <h4 className='text-[#FF834B] font-bold pt-5 pb-3'>
+          <div className="ebook_services w-[250px] h-64 p-5 shadow-md  border-2 rounded-xl border-[#FF834B]">
+            <img src="../images/orange file.png" alt="" />
+            <h4 className="text-[#FF834B] font-bold pt-5 pb-3">
               Ebook Writing
             </h4>
-            <p className='text-[#999999] pb-5'>
+            <p className="text-[#999999] pb-5">
               Lörem ipsum polysam spetila pyde. Trapolig regnbågsbarn och beligt
               ehylig viktigt
             </p>
-            <a href=''>
-              <img src='../images/orange arrow.png' alt='' />
+            <a href="">
+              <img src="../images/orange arrow.png" alt="" />
             </a>
           </div>
-          <div className=' article_writing w-[250px] h-64 p-5 border-2 rounded-xl border-[#FF9318]'>
-            <img src='../images/yellow file.png' alt='' />
-            <h4 className='text-[#FF9318] font-bold pt-5 pb-3'>
+          <div className=" article_writing w-[250px] h-64 p-5 border-2 shadow-md  rounded-xl border-[#FF9318]">
+            <img src="../images/yellow file.png" alt="" />
+            <h4 className="text-[#FF9318] font-bold pt-5 pb-3">
               Article Writing
             </h4>
-            <p className='text-[#999999] pb-5'>
+            <p className="text-[#999999] pb-5">
               Lörem ipsum polysam spetila pyde. Trapolig regnbågsbarn och beligt
               ehylig viktigt
             </p>
-            <a href=''>
-              <img src='../images/yellow arrow.png' alt='' />
+            <a href="">
+              <img src="../images/yellow arrow.png" alt="" />
             </a>
           </div>
-          <div className=' content_writing w-[250px] h-64 p-5 border-2 rounded-xl border-[#5252FF]'>
-            <img src='../images/purple file.png' alt='' />
-            <h4 className='text-[#5252FF] font-bold pt-5 pb-3'>
+          <div className=" content_writing w-[250px] h-64 p-5 border-2 shadow-md  rounded-xl border-[#5252FF]">
+            <img src="../images/purple file.png" alt="" />
+            <h4 className="text-[#5252FF] font-bold pt-5 pb-3">
               Content Writing
             </h4>
-            <p className='text-[#999999] pb-5'>
+            <p className="text-[#999999] pb-5">
               Lörem ipsum polysam spetila pyde. Trapolig regnbågsbarn och beligt
               ehylig viktigt
             </p>
-            <a href=''>
-              <img src='../images/purple arrow.png' alt='' />
+            <a href="">
+              <img src="../images/purple arrow.png" alt="" />
             </a>
           </div>
-          <div className=' ebook_services w-[250px] h-64 p-5 border-2 rounded-xl border-[#E72AFF]'>
-            <img src='../images/pink file.png' alt='' />
-            <h4 className='text-[#E72AFF] font-bold pt-5 pb-3'>
+          <div className=" ebook_services w-[250px] h-64 p-5 border-2  shadow-md  rounded-xl border-[#E72AFF]">
+            <img src="../images/pink file.png" alt="" />
+            <h4 className="text-[#E72AFF] font-bold pt-5 pb-3">
               Ebook Writing
             </h4>
-            <p className='text-[#999999] pb-5'>
+            <p className="text-[#999999] pb-5">
               Lörem ipsum polysam spetila pyde. Trapolig regnbågsbarn och beligt
               ehylig viktigt
             </p>
-            <a href=''>
-              <img src='../images/pink arrow.png' alt='' />
+            <a href="">
+              <img src="../images/pink arrow.png" alt="" />
             </a>
           </div>
         </section>
